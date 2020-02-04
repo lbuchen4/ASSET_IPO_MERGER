cf054f972c9a961da0756d567845a09f122160b4
Calum Smeaton <smeatsbeats@gmail.com>
2020-02-04 15:09:34 -0500

    Welcome to Weenie Hut baby

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
-
+    <h1><?php echo "Howdy." ?></h1>
+    <h2>Welcome to Weenie Hut Junior.</h2>
   </body>
 </html>
-<?php
-echo "Greetings";
-?>
