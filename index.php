20c63b29a0579833f568fa6b2c180f5fb4c9763d
Calum Smeaton <smeatsbeats@gmail.com>
2020-02-11 13:20:18 -0500

    welcome from Salvadore

--- index.php
+++ index.php
@@ -6,7 +6,59 @@
   </head>
   <body>
     <img src="images/weenie_hut_juniors.webp" alt="best restaurant ever">
-    <h1><?php echo "Howdy." ?></h1>
-    <h2>Welcome to Weenie Hut Junior. Have a hot dog.</h2>
+    <section id="welcome">
+      <h1><?php echo "Howdy." ?></h1>
+      <h2>Welcome to Weenie Hut Junior's. Have a hot dog.</h2>
+      <p>
+        If you are on this amazing webpage, then you have expressed interest in coding at some point in time. <br>
+        In case this website has not already convinced you of my prowess, I will simply confirm, I am a coding Jedi. <br>
+        Still not convinced? Watch this. <strong>Boom.</strong> Regard me please. <strong>Boom.</strong> <br>
+        That's right. It is I, Salvadore. <br>
+       </p>
+         <img src="images/salvadore.jpg" alt="master of sexy yoga and LAMP Stack expert">
+       <p>
+         Just because I tech sexy yoga you don't think I am capable of the coding? <br>
+         <em>Fuck you.</em> Regard me please. Coding is basically sexy yoga for your computer machine. <br>
+         And I am going to provide this safe space for all of us to practice, learn, grow and <strong>Boom</strong> together. <br>
+      </p>
+    </section>
+    <section class="languages">
+      <h2>Languages</h2>
+      <p>
+        The world of code can be overwhelming. Just like sexy yoga, the key is to breathe, and focus. <br>
+        We will be focusing primarily on:
+      </p>
+      <ul>
+        <li class="client">HTML</li>
+        <li class="client">CSS</li>
+        <li class="client">Javascript</li>
+        <li class="server">PHP</li>
+        <li class="server">MYSQL</li>
+      </ul>
+      <p>
+        The <span class="client">maroon</span> ones are known as <em>client-side</em> languages. <br>
+        They are interpretted by the web browser like chrome or firefux. <br>
+        The <span class="server">blue</span> ones are called <em>server-side</em> languages. <br>
+        Their functionality depends on little Chinese children in my basement. <br>
+      </p>
+    </section>
+    <section class="tools">
+      <h2>Tools</h2>
+      <p>Before we get dive in, we will need some tools to assist us on our journey.</p>
+      <ul>
+        <li>A computer machine.</li>
+        <li>A Text Editor - to type our code in.</li>
+        <li>Xampp - to communicate with the Chinese children.</li>
+        <li>Access to your command line - to feel like a legit hacker and use git.</li>
+        <li>A Github account - no relation to "the Hub" hub.</li>
+      </ul>
+      <p>There are a few other components to this setup that will make you want to quit before you start:</p>
+      <ul>
+        <li>AWS Elastic Beanstalk</li>
+        <li>Ngrok</li>
+      </ul>
+      <p>Best option might be to bring me your computer machine and I walk through it with you:</p>
+      <img src="images/boom.jpg" alt="boom">
+    </section>
   </body>
 </html>
